edf27078f77a4d41631696a9a25adc2ad53249b8
Jewel <jewelbansah@rocketmail.com>
2024-04-13 06:51:00 +0000

    fix allowed host

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-e6-+_#g)wdcdj&6(hnom$@d4j%gl#p95l93+*d5!-)c^tl7$j^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-b4ef.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
